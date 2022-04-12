68505917afc728caf6598ca3405e4a3a7b54507e
Albert Barsamov <albert.barsamov@gmail.com>
2022-04-12 17:03:07 +0200

    PropTypes of Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,10 +1,19 @@
-import s from './Container.module.css'
+import PropTypes from 'prop-types'
 import cn from 'classnames'
 
-export default function Container({ children, className, ...rest }) {
+import s from './Container.module.css'
+
+export default function Container({
+    children,
+    className
+}) {
     return (
         <div className={cn(s.root, className)}>
             {children}
         </div>
     )
+}
+Container.propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string
 }
