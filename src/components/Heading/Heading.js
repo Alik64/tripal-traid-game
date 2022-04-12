ed81b352bb6777f10063a4b621a5b3a2e1e2f9b2
Albert Barsamov <albert.barsamov@gmail.com>
2022-04-12 12:21:05 +0200

    02.02. Folder structure

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,7 @@
+import s from './Heading.module.css'
+
+export default function Heading() {
+    return (
+        <h1 className={s.root}>Wats'up !</h1>
+    )
+}
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,16 +1,17 @@
 import style from './Slider.module.css'
 import logoPng from '../../assets/Triple.png'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
+import { Heading } from '../Heading'
 
 export default function Slider() {
     return (
         <section className={style.section}>
             <div className={style.slider}>
                 <div className={`${style.container} ${style.sliderContent}`}>
-                    <h1 className={style.header}>Wow</h1>
+                    <Heading />
                     <h2 className={style.subheader}>Wow.Wow.Wow</h2>
-                    <div className={style.image} style={{ backgroundImage: `url(${logoPng})` }}>
-                        <Logo />
+                    <div className={style.image} >
+                        <img src={logoPng} alt="Logo" />
                     </div>
 
                     <div className={style.call}>
