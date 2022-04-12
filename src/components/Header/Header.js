2bc54c0116bca10b84376499018c83039e3b455a
Albert Barsamov <albert.barsamov@gmail.com>
2022-04-12 17:02:44 +0200

    Header + Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
+
 import Container from '../Container'
 import style from './Header.module.css'
+
+import logoPng from '../../assets/triple.png'
+
 
 export default function Header() {
     const MENU = ['Menu 1', 'Menu 2', 'Menu 3', 'Menu 4']
     return (
         <header className={style.root}>
             <div className={style.header}>
-                <Container>
-
-                    <div className={style.logo}></div>
+                <Container className={style.headerWrap}>
+                    <div className={style.logo}>
+                        <img src={logoPng} alt="logo" />
+                    </div>
                     <ul className={style.nav}> {MENU.map((item, index) => (
                         <li key={index}><a href="#">{item}</a></li>
                     ))}
