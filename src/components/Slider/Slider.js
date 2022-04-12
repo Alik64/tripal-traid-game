0a0fbe5177f348a264b60d8986209f1755285ebd
Albert Barsamov <albert.barsamov@gmail.com>
2022-04-12 07:48:10 +0200

    Using SVG ReactComponent

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,6 @@
 import style from './Slider.module.css'
 import logoPng from '../../assets/Triple.png'
+import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 export default function Slider() {
     return (
@@ -9,8 +10,10 @@ export default function Slider() {
                     <h1 className={style.header}>Wow</h1>
                     <h2 className={style.subheader}>Wow.Wow.Wow</h2>
                     <div className={style.image}>
-                        <img src={logoPng} alt="game" />
+
+                        <Logo />
                     </div>
+
                     <div className={style.call}>
                         <button className={style.button}>Wow</button>
                     </div>
