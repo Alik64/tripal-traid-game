090feceaf7e3ade5e0369793d91531fdfb66c3b4
Alik64 <albert.barsamov@gmail.com>
2022-04-20 23:27:34 +0200

    04.04 - Методы жизненного цикла классового компонента

--- src/Clock.js
+++ src/Clock.js
@@ -5,24 +5,42 @@ class Clock extends React.Component {
 
     constructor(props) {
         super(props);
+        console.log('constructor')
 
         this.state = {
             date: new Date().toLocaleTimeString()
         }
     }
+    // shouldComponentUpdate( nextProps, nextState, nextContext) {}
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        // console.log('old state', this.state)
+        // console.log('new state', nextState)
+        console.log('shouldComponentUpdate')
+        return true
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate')
+    }
+
     componentDidMount() {
+
         this.interval = setInterval(() => {
             console.log('time')
             this.setState({
                 date: new Date().toLocaleTimeString()
             })
         }, 1000)
+        console.log('componentDidMount')
+
     }
 
     componentWillUnmount() {
         clearInterval(this.interval)
+        console.log('componentWillUnmount')
+
     }
     render() {
+        console.log('render')
 
         return (
             <div>
