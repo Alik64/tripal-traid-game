37d701f2bc821251c4a6e3e0d0a060166f425e27
Alik64 <albert.barsamov@gmail.com>
2022-04-21 11:32:48 +0200

    Delete unnecessary comments in Clock.js

--- src/Clock.js
+++ src/Clock.js
@@ -1,55 +1,6 @@
-import { clear } from "@testing-library/user-event/dist/clear";
 import React, { useEffect, useState } from "react";
 
-// class Clock extends React.Component {
 
-//     constructor(props) {
-//         super(props);
-//         console.log('constructor')
-
-//         this.state = {
-//             date: new Date().toLocaleTimeString()
-//         }
-//     }
-//     // shouldComponentUpdate( nextProps, nextState, nextContext) {}
-//     shouldComponentUpdate(nextProps, nextState, nextContext) {
-//         // console.log('old state', this.state)
-//         // console.log('new state', nextState)
-//         console.log('shouldComponentUpdate')
-//         return true
-//     }
-//     componentDidUpdate(prevProps, prevState, snapshot) {
-//         console.log('componentDidUpdate')
-//     }
-
-//     componentDidMount() {
-
-//         this.interval = setInterval(() => {
-//             console.log('time')
-//             this.setState({
-//                 date: new Date().toLocaleTimeString()
-//             })
-//         }, 1000)
-//         console.log('componentDidMount')
-
-//     }
-
-//     componentWillUnmount() {
-//         clearInterval(this.interval)
-//         console.log('componentWillUnmount')
-
-//     }
-//     render() {
-//         console.log('render')
-
-//         return (
-//             <div>
-//                 The time is : {this.state.date}
-//             </div>
-//         )
-//     }
-
-// }
 
 const Clock = () => {
     console.log('render')
