1debcf5db2239e811588016172231796c8fe4990
Alik64 <albert.barsamov@gmail.com>
2022-04-28 00:56:32 +0200

    delete comments Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,32 +3,6 @@ import PropTypes from "prop-types";
 import cn from "classnames";
 import s from "./Button.module.scss";
 
-// const Button = ({ title, light, className, onBtnClick }) => {
-//   function clickHandler() {
-//     onBtnClick();
-//   }
-
-//   return (
-//     <button
-//       className={cn(s.root, className, { [s.light]: light })}
-//       onClick={clickHandler}
-//     >
-//       {title}
-//     </button>
-//   );
-// };
-
-// Button.defaultProps = {
-//   light: false,
-// };
-// Button.propTypes = {
-//   title: PropTypes.string,
-//   light: PropTypes.bool,
-//   className: PropTypes.string,
-//   onBtnClick: PropTypes.func,
-// };
-// export default Button;
-
 const Button = ({ onClick, children, color }) => {
   const handleClick = () => {
     onClick && onClick();
