3e730841184b598eb5bb77965139a3c0f55cbf11
Alik64 <albert.barsamov@gmail.com>
2022-06-15 12:17:16 +0200

    impliment initial input changings

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,53 +1,50 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
-
-import s from "./Input.module.scss";
 import cn from "classnames";
 
+import s from "./Input.module.scss";
+
 const Input = ({
-  label,
   type,
-  value,
-  name,
-  id,
-  placeholder,
   required,
+  label,
+  name,
   onChange,
+  defaultValue,
   className,
 }) => {
+  const [value, setValue] = useState(defaultValue ? defaultValue : "");
+
   const handleChange = (e) => {
+    setValue(e.target.value);
     onChange && onChange(e);
   };
 
   return (
-    <div className={cn(s.root, s[className])}>
+    <div className={cn(s.root, className)}>
       <input
-        className={cn(s[className])}
         type={type}
         name={name}
-        value={value}
-        onChange={(e) => handleChange(e)}
-        id={id}
         required={required}
-        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
       />
-      {label ? <label htmlFor={id}>{label}</label> : null}
-      <div className={s.bar}></div>
+      <label className={cn({ [s.valid]: value.length > 0 })}>{label}</label>
+      <div className={s.bar} />
     </div>
   );
 };
 
 Input.defaultProps = {
-  placeholder: " ",
-  required: true,
+  type: "text",
+  required: false,
 };
 Input.propTypes = {
-  label: PropTypes.string,
-  type: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  name: PropTypes.string,
-  id: PropTypes.string,
-  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(["text", "email", "password", "number"]),
+  defaultValue: PropTypes.string,
   required: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string,
   onChange: PropTypes.func,
   className: PropTypes.string,
 };
